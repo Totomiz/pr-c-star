0b2275689533dbc123122d8228872e5657e7eb79
solt <solt@soltdeiMac.local>
2021-09-26 19:12:06 +0800

    init

--- grammar/ls14-preprocessor.c
+++ grammar/ls14-preprocessor.c
@@ -16,14 +16,23 @@
         return x>y?x:y;   \
         }
 
+
 GENRIC_MAX(float)
 
 GENRIC_MAX(int)
 
+//可变参数宏
+#define  TEST(condition, ...) ((condition)?printf("test past %s\n",#condition):printf(__VA_ARGS__))
 
+//C99 中__func__标识符,属于新特性，与预处理无关，实际不属于本节内容，相当于在每个函数前定义 static char args[]="function-name"
+#define FUNCTION_CALLED() printf("%s function called:\n",__func__)
+#define FUNCTION_RETURNED() printf("%s function returned!\n",__func__)
+
+#define DEBUG 1
 
 
 int main(int argc, char *argv[]) {
+    FUNCTION_CALLED();
     PRINT_T(10);
     int a = MAX(1, 2020);
     PRINT_T(a);
@@ -31,7 +40,38 @@ int main(int argc, char *argv[]) {
     dMY(float_max(1100.0f, 100.0f));
     PRINT_T(int_max(1100.0f, 100.0f));
     //预定义宏 __LINE__、__FILE__\__DATE__\__TIME__\ __STDC__
-    printf("this line is %d file= %s,date=%s,time=%s,STDC=%d\n", __LINE__, __FILE__, __DATE__, __TIME__, __STDC__);
+    printf("this line is %d file= %s,date=%s,time=%s,STDC=%d STDC_HOSTED=%d\n", __LINE__, __FILE__, __DATE__, __TIME__,
+           __STDC__, __STDC_HOSTED__);
+
+    TEST(100 > 0, "this is pass condition");
+    TEST(100 > 200, "this is un pass condition by %d > %d\n", 100, 200);
+
+    //如 debug 为0 预处理器会删除 #if和#endif 间的这段代码
+#if DEBUG
+    printf("debug value of date=%s \n", __DATE__);
+#endif
+
+    //defined 运算符，应用于标识符时，如果是一个定义过的宏则返回1，否者返回0，通常与#if #endif结合使用
+#if defined(DEBUG)
+    printf("#if defined(DEBUG) output\n");
+#endif
+
+    //#ifdef 测试一个标识是否已经被宏定义
+#ifdef DEBUG
+    printf("#ifdef DEBUG output\n");
+#endif
+
+    //#ifdef 测试一个标识是否未被宏定义
+#ifndef DEBUG
+    printf("#ifndef DEBUG output\n");
+#endif
+    //#elif #else 相当于写条件语句，如条件编译macos,win,linux不同平台对应不同实现
+#if DEBUG
+#elif DEBUG
+#else DDEBUG
+#endif
+
+    FUNCTION_RETURNED();
     return 0;
 }
 
--- grammar/ls16_struct.c
+++ grammar/ls16_struct.c
@@ -2,3 +2,53 @@
 // Created by solt on 2021/9/24.
 //
 
+#include <stdio.h>
+
+
+//直接定义名字
+struct PointZT {
+    int x;
+    int y;
+    char *name;
+} A = {.name="aaab", .x=1, .y=2}, B = {.name="BBB", .x=1, .y=5};
+
+
+// typedef 方式定义的方式名字放在末尾，同理typedef type name;
+typedef struct {
+    int w;
+    int h;
+    char *name;
+} Rect;
+
+//struct做为参数
+void area(struct PointZT pointZt) {
+    printf("this area point x=%d y = %d\n", pointZt.x, pointZt.y);
+}
+//struct作为返回值
+
+struct PointZT funcGenPoint(int x, int y, char *name) {
+    struct PointZT a = {.x=x, .y=y, .name=name};
+    return a;
+}
+
+#define PintPoint(expr, x, y) printf(#expr " of value x = %d y=%d",x,y )
+
+int main() {
+//    A.name="AAA";
+//    A.x=100;
+//    A.y=2000;
+    printf("name of a %s", A.name);
+    printf("sum of a = %d", A.x + A.y);
+    printf("\n");
+    printf("name of b %s", B.name);
+    printf("sum of b = %d", B.x + B.y);
+    //使用typedef方式声明struct的局部变量定义,前面不需加struct关键字
+    Rect a = {.w=200, .h=100, .name="rectA"};
+    Rect b = {.w=200, .h=100, .name="rectA"};
+    struct PointZT pointZtA = {.name="aac", .x=1, .y=2};
+    printf("sum of RectA = %d", a.w + a.h);
+    printf("sum of pointZtA = %d", pointZtA.x + pointZtA.y);
+    area(pointZtA);
+    struct PointZT genPoint = funcGenPoint(10, 40, "genPoint");
+    PintPoint(funcGenPoint(10, 40, "genPoint"), genPoint.x, genPoint.y);
+}
